fb63189fbeb173f83cd1fde8af350cb799c2e783
Hiruni Daas Bandara <144683712+Hirunidb@users.noreply.github.com>
2026-07-09 03:03:56 +0100

    File path updatd

--- 0.2_filterSNPcpg.R
+++ 0.2_filterSNPcpg.R
@@ -1,8 +1,9 @@
-#setwd("/SAN/ghlab/epigen/Hiruni")
-setwd("/Users/hirunidb/Library/CloudStorage/OneDrive-Personal/Projects/Hellenthal-SilverLab_LIDo/hellenthalSilverLab_DNAm_pvt/")
+### Filtering out CpGs that overlay single nucleotide polymorphisms (SNPs) on the genome
 
-cpgLoci <- read.csv('hannon-mill/loyfer_WGBS_rawData/CpG.bed.gz', sep = "\t", header = FALSE)
-snpLoci <- read.csv('metadata/0.1_snps_maf_0.01_chr_pos.txt', header=FALSE, sep='_') # SNP positions
+setwd("/path/to/project/folder/")
+
+cpgLoci <- read.csv('CpG.bed.gz', sep = "\t", header = FALSE) # all CpG positions on the chromosomes across the genome
+snpLoci <- read.csv('0.1_snps_maf_0.01_chr_pos.txt', header=FALSE, sep='_') # SNP positions
 cpgLoci_f <- cpgLoci[!(cpgLoci[,2] %in% snpLoci[,2]), ]
 
-write.csv(cpgLoci_f, 'metadata/0.2_CpG_filt.csv')
+write.csv(cpgLoci_f, '0.2_CpG_filt.csv')
